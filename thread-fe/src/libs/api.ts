e0e6a96869bd8d3366d235084d37c1082e004c45
BagasSatr3 <bagassatjin23@gmail.com>
2023-10-18 09:52:04 +0700

    fix: fix edit profile

--- thread-fe/src/libs/api.ts
+++ thread-fe/src/libs/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1/"
+    baseURL: "https://b48-s2-production.up.railway.app/"
 })
 
 export function setAuthToken(token: string) {
